3d35dffd220cc24337d68f67186669ca5a62eac2
App Academy Student <student@appacademy.io>
2014-03-19 10:45:15 -0700

    Adding static home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Sharebnb::Application.routes.draw do
-  root to: "sessions#new"
+  root to: "static_pages#root"
+  get "static_pages/root"
+
+  namespace :api, defaults: {format: :json} do
+  end
+
   resources :users, only: [:new, :create]
   resource :sessions, only: [:new, :create, :destroy]
 end
