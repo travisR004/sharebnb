bed0ecdf7fba51b31d8378efb88e97f4057d46fd
Travis Randolph <travisR004@gmail.com>
2014-03-25 13:42:48 -0700

    messages working

--- app/assets/javascripts/views/made_request.js
+++ app/assets/javascripts/views/made_request.js
@@ -1,45 +1,42 @@
 window.Sharebnb.Views.MadeRequest = Backbone.View.extend({
 	initialize: function(){
-		this.listenTo(this.model, "all", this.render);
-		this.listenTo(this.model.messages(), "add", this.render)
+		this.listenTo(this.model, "sync", this.render);
+		this.listenTo(this.model.messages(), "sync", this.render)
+		this.rental = Sharebnb.Data.rentals.getOrFetch(this.model.get("rental_id"))
 	},
 
 	template: JST["rental_request/made_request"],
 
 	events: {
 		"click #open-compose-message": "toggleMessageComposer",
-		"click #submit-message": "createMessage"
+		"submit .message-form": "createMessage"
 	},
 
 	toggleMessageComposer: function(event){
 		event.preventDefault();
 		$("#open-compose-message").toggle();
-		$("#message-form").toggle("slow")
-	},
-
-	closeMessageBox: function(event){
-		event.preventDefault();
-		$("#open-compose-message").toggle("slow");
-		$("#message-form").toggle()
+		$("#request-message-form" + this.model.id).toggle("slow")
 	},
 
 	createMessage: function(event){
 		var that = this;
 		event.preventDefault();
-		this.closeMessageBox(event);
-		var messageData = $(event.currentTarget).parent().serializeJSON();
+		var messageData = $(event.target).serializeJSON();
 		messageData.message.rental_request_id = this.model.id;
 		var receiver_id = Sharebnb.Data.rentals.get(this.model.get("rental_id")).get("owner_id");
 		messageData.message.receiver_id = receiver_id;
 		$("made-request-message-modal" + that.model.id).modal("show");
 		$('.modal-backdrop').remove();
+		this.openModal = true;
 		this.model.messages().create(messageData);
 	},
 
 	render: function(){
-		var rental = Sharebnb.Data.rentals.getOrFetch(this.model.get("rental_id"))
-		var renderedContent = this.template({request: this.model, rental: rental, messages: this.model.messages() });
+		var renderedContent = this.template({request: this.model, rental: this.rental, messages: this.model.messages() });
 		this.$el.html(renderedContent);
+		if(this.openModal){
+			this.$el.find("#made-request-messages").click()
+		}
 		return this;
 	}
 })
--- app/assets/javascripts/views/request_response.js
+++ app/assets/javascripts/views/request_response.js
@@ -5,6 +5,7 @@ window.Sharebnb.Views.RequestResponse = Backbone.View.extend({
 		this.requestor = new Sharebnb.Collections.Users().getOrFetch(this.model.get("user_id"))
 		this.rental = Sharebnb.Data.rentals.getOrFetch(this.model.get("rental_id"))
 		this.listenTo(this.requestor, "sync", this.render)
+		this.listenTo(this.model.messages(), "sync", this.render)
 	},
 
 	template: JST["rental_request/response"],
@@ -16,12 +17,35 @@ window.Sharebnb.Views.RequestResponse = Backbone.View.extend({
 																				 messages:this.model.messages()
 																			 });
 		this.$el.html(renderedContent);
+		if(this.openModal){
+			this.$el.find("#messages").click()
+		}
 		return this;
 	},
 
 	events: {
 		"click #request-accept": "approveRequest",
-		"click #request-deny": "denyRequest"
+		"click #request-deny": "denyRequest",
+		"click #open-response-message": "toggleMessageComposer",
+		"submit .message-form": "createMessage"
+	},
+
+	toggleMessageComposer: function(event){
+		event.preventDefault();
+		$("#open-response-message").toggle();
+		$("#message-form" + this.model.id).toggle("slow")
+	},
+
+	createMessage: function(){
+		var that = this;
+		event.preventDefault();
+		var messageData = $(event.target).serializeJSON();
+		messageData.message.rental_request_id = this.model.id;
+		messageData.message.receiver_id = this.model.escape("user_id");
+		$("made-request-message-modal" + that.model.id).modal("show");
+		$('.modal-backdrop').remove();
+		this.openModal = true;
+		this.model.messages().create(messageData);
 	},
 
 	approveRequest: function(event){
