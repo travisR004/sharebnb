43e6bb5c9f9cf9366e7d3ab8312f6ff2c1587378
Travis <travisR004@gmail.com>
2014-04-05 22:33:17 -0700

    snazzier map

--- app/assets/javascripts/views/search.js
+++ app/assets/javascripts/views/search.js
@@ -103,39 +103,58 @@ window.Sharebnb.Views.SearchResult = Backbone.View.extend({
   makeMap: function(){
     var that = this;
 		style = [
-			{
+      {
         "featureType": "water",
-        "elementType": "all",
         "stylers": [
-            { "hue": "#1CB2BD" },
-            { "saturation": 53 },
-            { "lightness": -44 },
-            { "visibility": "on" }
-        ]
-	    },
-      {
-        "featureType": "road",
-        "elementType": "all",
-        "stylers": [
-            { "hue": "#1CB2BD" },
-            { "saturation": 40 }
-        ]
-		  },
-      {
-        "featureType": "landscape",
-        "elementType": "all",
-        "stylers": [
-            { "hue": "#BBDC00" },
-            { "saturation": 80 },
-            { "lightness": -20 },
+            { "color": "#46bcec" },
             { "visibility": "on" }
         ]
       },
       {
+        "featureType": "landscape",
+        "stylers": [
+            { "color": "#f2f2f2" }
+        ]
+      },
+      {
+        "featureType": "road",
+        "stylers": [
+            { "saturation": -100 },
+            { "lightness": 45 }
+        ]
+      },
+      {
         "featureType": "road.highway",
-        "elementType": "all",
-        "stylers": [ { "visibility": "on"}]
-			}
+        "stylers": [
+            { "visibility": "simplified"}
+        ]
+      },
+      {
+        "featureType": "road.arterial",
+        "elementType": "labels.icon",
+        "stylers": [
+            { "visibility": "off" }
+        ]
+      },
+      {
+        "featureType": "administrative",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            { "color": "#444444" }
+        ]
+      },
+      {
+        "featureType": "transit",
+        "stylers": [
+            { "visibility": "off" }
+        ]
+      },
+      {
+        "featureType": "poi",
+        "stylers": [
+            { "visibility": "off" }
+        ]
+      }
     ]
     var mapOptions = {
       center: new google.maps.LatLng(this.lat, this.long),
